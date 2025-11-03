ef057cc175776c20732f616e813e209b5e0ac9c1
mridul sharma <Mridulsharma3130@gmail.com>
2025-11-04 00:03:30 +0530

    Refactor app.js: clean up commented code and improve middleware organization

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,18 +5,8 @@ import { globalErrorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-// Middleware for accepting json
+//* Middleware 
 app.use(express.json({ limit: "32kb" }));
-
-// app.post("/register", (req, res) => {
-//     console.log("Req : ", req.body);
-
-//     registerUser(req , res)
-//         .catch((err) => {
-//             logger.error( "Error /register" ,  err);
-//             res.status(400).json(err);
-//         });
-// });
 
 //*********** importing router **********
 import userRouter from "./routes/user.routes.js";
@@ -31,6 +21,8 @@ app.use("/api/v1/users" , userRouter)
 
 
 
+
+//! this should be at the end to function properly
 app.use(globalErrorHandler);
 
 export { app };
