e16805b597121529b763886603894c2e12cd32d6
Manmohit Singh <manmohit249@gmail.com>
2025-11-05 16:52:52 +0530

    added start script to package.json and updated .env.sample

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,26 +1,16 @@
 import express from "express";
-import { registerUser } from "./controllers/user.controller.js";
-import { logger } from "./utils/index.js";
 import { globalErrorHandler } from "./middlewares/errorHandler.js";
 
 const app = express();
 
-//* Middleware 
+//* Middleware
 app.use(express.json({ limit: "32kb" }));
 
 //*********** importing router **********
 import userRouter from "./routes/user.routes.js";
 
-
 //*********** router declaration ********
-app.use("/api/v1/users" , userRouter)
-
-
-
-
-
-
-
+app.use("/api/v1/users", userRouter);
 
 //! this should be at the end to function properly
 app.use(globalErrorHandler);
